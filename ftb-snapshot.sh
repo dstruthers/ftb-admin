de7d4752509ba2e3fed58c84cb71d5d16479fca7
Darren Struthers <dstruthers@gmail.com>
2013-03-08 11:52:59 -0800

    Added hourly snapshot process

---
+++ ftb-snapshot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FTB_DIR=/opt/ftb
+FTB_BACKUP_DIR=/home/minecraft/ftb-backups
+FTB_RSYNC_DEST=/media/robertsegal/Minecraft
+
+if ftb status > /dev/null; then
+    snapshot_file="$FTB_BACKUP_DIR/hourly-snapshot.tar.gz"
+    cd $FTB_DIR && tar cvzfp $snapshot_file .
+    echo "$(date): Created hourly snapshot at $snapshot_file\n"
+else
+    echo "$(date): FTB server is not running. Skipping hourly snapshot.\n"
+fi
