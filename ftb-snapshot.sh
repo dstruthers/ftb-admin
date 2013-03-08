6d326edf432d0adbc2cabd70398934d8c6127bb6
Darren Struthers <dstruthers@gmail.com>
2013-03-08 11:58:52 -0800

    Added rsync to NAS as part of hourly snapshot process

--- ftb-snapshot.sh
+++ ftb-snapshot.sh
@@ -7,6 +7,7 @@ FTB_RSYNC_DEST=/media/robertsegal/Minecraft
 if ftb status > /dev/null; then
     snapshot_file="$FTB_BACKUP_DIR/hourly-snapshot.tar.gz"
     cd $FTB_DIR && tar cvzfp $snapshot_file .
+    rsync -rlD --update $FTB_BACKUP_DIR $FTB_RSYNC_DEST
     echo "$(date): Created hourly snapshot at $snapshot_file\n"
 else
     echo "$(date): FTB server is not running. Skipping hourly snapshot.\n"
